d8b423f492f8e82c2ed42e784aa910d8169ee8f3
Prathik Raj <prathik011@gmail.com>
2017-06-12 20:52:46 +0530

    output.txt and ability to diff with existing output

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,41 +4,41 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = Generator.extend({
-    prompting: function () {
-        // Have Yeoman greet the user.
-        this.log(yosay(
-            'Welcome to the dandy ' + chalk.red('generator-codechef') + ' generator!'
-        ));
+  prompting: function() {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the dandy ' + chalk.red('generator-codechef') + ' generator!'
+    ));
 
-        var prompts = [{
-            type: 'input',
-            name: 'projectName',
-            message: 'Name of your project?',
-            default: this.appname
-        }];
+    var prompts = [{
+      type: 'input',
+      name: 'projectName',
+      message: 'Name of your project?',
+      default: this.appname
+    }];
 
-        return this.prompt(prompts).then(function (props) {
-            // To access props later use this.props.someAnswer;
-            this.props = props;
-        }.bind(this));
-    },
+    return this.prompt(prompts).then(function(props) {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    }.bind(this));
+  },
 
-    writing: function () {
-        this.fs.copy(
-            this.templatePath('main.cpp'),
-            this.destinationPath(this.props.projectName + '/main.cpp')
-        );
-        this.fs.copy(
-            this.templatePath('Makefile'),
-            this.destinationPath(this.props.projectName + '/Makefile')
-        );
-        this.fs.copy(
-            this.templatePath('test.txt'),
-            this.destinationPath(this.props.projectName + '/test.txt')
-        );
+  writing: function() {
 
-    },
+    this.fs.copy(
+      this.templatePath('main.cpp'),
+      this.destinationPath(this.props.projectName + '/main.cpp')
+    );
+    this.fs.copy(
+      this.templatePath('Makefile'),
+      this.destinationPath(this.props.projectName + '/Makefile')
+    );
+    this.fs.copy(
+      this.templatePath('test.txt'),
+      this.destinationPath(this.props.projectName + '/test.txt')
+    );
 
-    install: function () {
-    }
+  },
+
+  install: function() {}
 });
--- test/app.js
+++ test/app.js
@@ -3,16 +3,17 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-describe('generator-codechef:app', function () {
-  before(function () {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
-      .toPromise();
-  });
+beforeEach(function () {
+  return helpers.run(path.join(__dirname, '../generators/app'))
+    .withPrompts({ projectName: "test" });
+})
 
-  it('creates files', function () {
-    assert.file([
-      'dummyfile.txt'
-    ]);
-  });
+describe('generator-codechef:app', function () {
+    it('creates files', function () {
+        assert.file([
+            'test/test.txt',
+            'test/main.cpp',
+            'test/Makefile'
+        ]);
+    });
 });
