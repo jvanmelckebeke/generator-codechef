49b5a5902d273ad18dc3862f41595d5c4d8cd89a
Prathik Raj <prathik011@gmail.com>
2017-06-12 21:46:52 +0530

    adding output.txt

--- generators/app/index.js
+++ generators/app/index.js
@@ -37,6 +37,11 @@ module.exports = Generator.extend({
       this.templatePath('test.txt'),
       this.destinationPath(this.props.projectName + '/test.txt')
     );
+    this.fs.copy(
+      this.templatePath('output.txt'),
+      this.destinationPath(this.props.projectName + '/output.txt')
+    );
+
 
   },
 
